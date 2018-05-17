acb7929a31c6718b57fb2aa44ad02628a61ea8e5
lokeshtechx <lokeshtechximum@gmail.com>
2018-05-17 17:18:19 +0530

    latest DB

--- DB/rim_dev.sql
+++ DB/rim_dev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2018 at 01:36 PM
+-- Generation Time: May 17, 2018 at 01:47 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
